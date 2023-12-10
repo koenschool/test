20b5c7cef78f4c5e24d29b6106a0388bcb76df8e
koen <9023005@student.zadkine.nl>
2023-12-10 09:34:52 +0100

    schaar update

--- schaar/schaar.js
+++ schaar/schaar.js
@@ -50,10 +50,51 @@ function schaar2(){
     verander2();
 }
 
+
+gewonnen="heeft gewonnen";
+gelijk="niemand heeft gewonnen want het is gelijk";
+
 function laatzien(){
-    if(getal2=="✂️"){
-        alert("omg dit werkt wat epic")
+    //dit gedeelte is voor de gelijke uitkomst:)
+    if(getal1=="🪨"&&getal2=="🪨"){
+        getal3="";
+        getal4 = gelijk;
     }
-    document.write("<style> h1{ font-family: monospace; color:blue;</style><h1>speler 1 deed",getal1,"en speler 2 deed",getal2,"</h1>")
+    else if(getal1=="📃"&&getal2=="📃"){
+        getal3="";
+        getal4 = gelijk;
+    }
+    else if(getal1=="✂️"&&getal2=="✂️"){
+        getal3="";
+        getal4 = gelijk;
+        //dit gedeelte is voor speler1
+        }
+        else if(getal1=="🪨"&&getal2=="✂️"){
+            getal3 = "speler1";
+            getal4 = gewonnen;
+        }
+        else if(getal1=="🪨"&&getal2=="📃"){
+            getal3 = "speler2";
+            getal4 = gewonnen;
+        }
+        else if(getal1=="✂️"&&getal2=="🪨"){
+            getal3 = "speler2";
+            getal4 = gewonnen;
+        }
+        else if(getal1=="✂️"&&getal2=="📃"){
+            getal3 = "speler1";
+            getal4 = gewonnen;
+        }
+        else if(getal1=="📃"&&getal2=="🪨"){
+            getal3 = "speler1";
+            getal4 = gewonnen;
+        }
+        else if(getal1=="📃"&&getal2=="✂️"){
+            getal3 = "speler2";
+            getal4 = gewonnen;
+        }
+
+
+    document.write("<style> h1{ font-family: monospace; color:blue;</style><h1>speler 1 deed",getal1,"en speler 2 deed",getal2," dus ",getal3," ",getal4,"</h1>")
     
 }
