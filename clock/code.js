9b1fbedcf37a7e81e640feeeeeff5ec7936dad8a
koen <9023005@student.zadkine.nl>
2024-10-27 11:32:11 +0100

    Update code.js

--- clock/code.js
+++ clock/code.js
@@ -53,10 +53,12 @@ $(document).ready(function () {
     }
 
     if (clock_sec) {
+        clock_sec++;
         $("#clock_min").css("rotate", ((clock_min * 6) + "deg"));
         clock_sec_test = setInterval(function () {
             // console.log("sec:", clock_sec, " min:", clock_min, " hr:", clock_hr);
-            $("#clock_dig").html(clock_hr + ":" + clock_min + ":" + clock_sec);
+            if (clock_sec < 10) { clock_sec_disp = "0" + clock_sec } else { clock_sec_disp = clock_sec }
+            $("#clock_dig").html(clock_hr + ":" + clock_min + ":" + clock_sec_disp);
             $("#clock_sec").css("rotate", ((clock_sec * 6) + "deg"));
             if (clock_sec >= 59) {
                 clock_sec = -1;
